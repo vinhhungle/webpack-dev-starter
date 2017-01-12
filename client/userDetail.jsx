a1a115ebc7a4ac90c34b5a75f5c33d9641de591b
Hung Le <vinhhung.le@gmail.com>
2017-01-12 12:46:52 +0700

    add log to see route props

--- client/userDetail.jsx
+++ client/userDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default class UserDetail extends React.Component {
   render() {
+    console.log('render UserDetail', this)
     return (
       <div>
         <h3>User Id: {this.props.params.userId}</h3>
