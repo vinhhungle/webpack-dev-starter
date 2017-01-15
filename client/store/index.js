9112e1da6dbe3cd602076f5304f9c01e3ab20116
Hung Le <vinhhung.le@gmail.com>
2017-01-15 12:10:14 +0700

    add thunk and logger middlewares for redux

--- client/store/index.js
+++ client/store/index.js
@@ -1,8 +1,16 @@
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+
 import rootReducer from './reducers'
+
+const logger = createLogger({
+  level: 'info',
+  collapsed: true
+})
 
 export default (app) => {
   app.config(($ngReduxProvider) => {
     'ngInject'
-    $ngReduxProvider.createStoreWith(rootReducer, ['ngUiRouterMiddleware'])
+    $ngReduxProvider.createStoreWith(rootReducer, ['ngUiRouterMiddleware', logger, thunk])
   })
 }
